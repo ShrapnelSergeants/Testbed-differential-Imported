51f79edfb74a9494b21d62a3c2b6890d1cf51d55
ShrapnelSergeants <team2408programming@gmail.com>
2024-04-10 14:47:27 -0500

    working on pathplanner methods

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -8,13 +8,29 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerTrajectory;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelPositions;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.Constants.PhysicalConstants;
 
@@ -35,6 +51,18 @@ public class Drivetrain extends SubsystemBase {
 
 
   private final AHRS gyro;
+
+  private DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(getAngle(),getLeftDistanceMeters(),getRightDistanceMeters());
+
+  private DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(Constants.PhysicalConstants.kTrackWidth);
+
+  private DifferentialDriveWheelPositions wheelPositions;
+
+  private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(0.22, 1.98, 0.2); //kS, kV, kA - get from other code
+  
+  private final PIDController leftPIDController = new PIDController(8.5,0,0); //update values
+  private final PIDController rightPIDController = new PIDController(8.5,0,0); //update values
+  private Field2d field = new Field2d();
 
   public Drivetrain() {
     //left side
@@ -61,6 +89,13 @@ public class Drivetrain extends SubsystemBase {
 
     backRightDrive.follow(frontRightDrive);
 
+
+    wheelPositions = new DifferentialDriveWheelPositions(
+                        getLeftDistanceMeters(),
+                        getRightDistanceMeters()
+    );
+
+
     //gyro
     gyro = new AHRS();
     
@@ -75,13 +110,31 @@ public class Drivetrain extends SubsystemBase {
 
     frontLeftEncoder.setDistancePerPulse(PhysicalConstants.kDistancePerPulse);
     frontRightEncoder.setDistancePerPulse(PhysicalConstants.kDistancePerPulse);
-
+/*
+    AutoBuilder.configureRamsete(
+      this::getPose,
+      this::resetPose,
+      this::getCurrentSpeeds,
+      this::drive,
+      new ReplanningConfig(),
+      () -> {
+        var alliance = DriverStation.getAlliance();
+        if (alliance.isPresent()) {
+            return alliance.get() == DriverStation.Alliance.Red;
+        }
+        return false;
+      }
+    
+      
+    )
+*/
 
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    odometry.update(getAngle(), getLeftDistanceMeters(), getRightDistanceMeters());
   }
 
 
@@ -96,10 +149,28 @@ public class Drivetrain extends SubsystemBase {
     differentialDrive.arcadeDrive(fwd, rot);
   }
 
+  public void tankDriveVolts(double leftVolts, double rightVolts) {
+    frontLeftDrive.setVoltage(leftVolts);
+    frontRightDrive.setVoltage(rightVolts);
+    differentialDrive.feed();
+  }
+
   /** Resets the drive encoders to currently read a position of 0. */
   public void resetEncoders() {
     frontLeftEncoder.reset();
     frontRightEncoder.reset();
+  }
+
+  public Rotation2d getAngle(){
+    return gyro.getRotation2d();
+  }
+
+  public double getLeftDistanceMeters(){
+    return frontLeftEncoder.getDistance();
+  }
+
+  public double getRightDistanceMeters(){
+    return frontRightEncoder.getDistance();
   }
 
   /**
@@ -120,6 +191,57 @@ public class Drivetrain extends SubsystemBase {
     differentialDrive.setMaxOutput(maxOutput);
   }
 
+  public PIDConstants getLeftPIDConstants(){
+    return new PIDConstants(8.5,0,0); //update
+
+  }
+
+  public PIDConstants getRightPIDConstants(){
+    return new PIDConstants(8.5,0,0); //update
+  }
+
+  public SimpleMotorFeedforward getSimpleMotorFeedforward() {
+    return feedforward;
+  }
+
+  public DifferentialDriveKinematics getKinematics() {
+    return kinematics;
+  }
+
+  public Pose2d getPose(){
+    return odometry.getPoseMeters();
+  }
+/*
+  public Pose2d resetPose(){
+    return odometry.resetPosition(getAngle(), wheelPositions, getPose());
+  }
+*/
+  public void resetOdometry(){
+    odometry.resetPosition(getAngle(), 0, 0, getPose());
+
+  }
+
+
+  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+    return new DifferentialDriveWheelSpeeds(frontLeftEncoder.getRate(), frontRightEncoder.getRate());
+  }
+
+  /*
+  public PPRamseteCommand createRamseteCommand(PathPlannerTrajectory trajectory){
+    return new PPRamseteCommand(
+      trajectory,
+      this::getPose(),
+      new RamseteController(2, 0.7), 
+      feedforward,
+      kinematics,
+      this::getWheelSpeeds,
+      leftPIDController,
+      rightPIDController,
+      this::tankDriveVolts,
+      this
+    );
+  }
+*/
 
   // look into this
   @Override
